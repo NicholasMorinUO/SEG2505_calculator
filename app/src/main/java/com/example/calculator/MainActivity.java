f044cfbd28e58a38bbc050de20336b4cd193014c
Wilfried SANKARA <wsank045@uottawa.ca>
2020-10-14 18:09:59 -0400

    design

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,8 +64,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View V){
                 checkCleared();
                 screen.setText(screen.getText()+"0");
+
             }
         });
+
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View V){
